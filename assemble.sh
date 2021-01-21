b59bad50f3e131a6f67618f7a5f96f763ce0fda9
bennyhuo <bennyhuo@tencent.com>
2021-01-21 15:59:47 +0800

    use ffmpeg in current directory.

--- assemble.sh
+++ assemble.sh
@@ -1,2 +1,11 @@
-pip3 install -r requirement.txt
+WORKING_DIR=$(cd $(dirname $0) && pwd -P)
+cd $WORKING_DIR
+pwd
+
+pip3 install -r requirements.txt
 pyinstaller jumpcutter.py --onefile
+
+# copy ffmpeg
+cp $(ls $(which ffmpeg)) dist/ 
+
+cd -
--- shell_utils.py
+++ shell_utils.py
@@ -1,11 +1,20 @@
 import subprocess
+import os, sys
+
+
+def get_script_path():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
+
 
 STRING = -1
+WORKING_DIR = get_script_path()
+ENV = os.environ.copy()
+ENV['PATH'] = f"{ENV['PATH']}{os.pathsep}{WORKING_DIR}"
 
 
 def do_shell(command, stdout=None, encoding=None):
     print(f"[Shell] {command}")
     if stdout == STRING:
-        return subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding=encoding).stdout
+        return subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding=encoding, cwd=WORKING_DIR, env=ENV).stdout
     else:
-        subprocess.run(command, shell=True, stdout=stdout, encoding=encoding)
+        subprocess.run(command, shell=True, stdout=stdout, encoding=encoding, cwd=WORKING_DIR, env=ENV)
