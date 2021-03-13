f587d07c79d02ff86791b55bf96a30cb2c7b67d7
bennyhuo <bennyhuo@kotliner.cn>
2021-03-13 09:00:28 +0800

    fix working dir problem. wrongly set the directory of jumpcutter executable as working dir.

--- shell_utils.py
+++ shell_utils.py
@@ -15,6 +15,6 @@ ENV['PATH'] = f"{ENV['PATH']}{os.pathsep}{WORKING_DIR}"
 def do_shell(command, stdout=None, encoding=None):
     print(f"[Shell] {command}")
     if stdout == STRING:
-        return subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding=encoding, cwd=WORKING_DIR, env=ENV).stdout
+        return subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding=encoding, env=ENV).stdout
     else:
-        subprocess.run(command, shell=True, stdout=stdout, encoding=encoding, cwd=WORKING_DIR, env=ENV)
+        subprocess.run(command, shell=True, stdout=stdout, encoding=encoding, env=ENV)
