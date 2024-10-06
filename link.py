c58432a60b2df2c882dc48cd3a479aff7012492f
bennyhuo <bennyhuo@kotliner.cn>
2024-10-06 22:29:23 +0800

    reduce start and end padding.

--- link.py
+++ link.py
@@ -35,7 +35,7 @@ short_cut = os.path.join(BIN_ROOT, f'jumpcut{file_ext}')
 with open(short_cut, 'w') as file:
     file.write(shebang_line)
     file.write(f'{link_file} --input_file {args1} --silent_speed 9999 --frame_margin 3 '
-               f'--keep_start 4 --keep_end 3 --use_hardware_acc 1 --silent_threshold 0.005\n')
+               f'--keep_start 2 --keep_end 2 --use_hardware_acc 1 --silent_threshold 0.005\n')
 
 os.chmod(link_file, stat.S_IREAD | stat.S_IEXEC | stat.S_IWUSR)
 os.chmod(short_cut, stat.S_IREAD | stat.S_IEXEC | stat.S_IWUSR)
